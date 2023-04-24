8a8a4a7533048e6d30608a7b32d7337633c0da28
CMLTTN <cemaldim@gmail.com>
2023-04-24 10:20:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+// let arr =["js","react","C++","CSS"];
+
+// arr.push("go");
+// // alert (arr.length);
+
+// for (let i=0;i<=15; i++){
+//      console.log(i);
+// }
+
+//  let i=0
+// while (i<=100){
+//      console.log("i değeri:"+i);
+//      i+=2;
+// }
+// function selamla (isim){
+//      console.log("methaba"  +isim);
+// }
+// selamla("selahaddin")
+
+// document.write("selam ve sevgiler <br>")
+// document.write("selam ve sevgiler")
+// console.log(document.location.host);
+// console.log(window);
+
+
+// // /*1     \'     '   Single quote        tek tırnak
+// // //*2     \"     "   Double quote        çift tırnak
+// // //*3      \\    \   Backslash            \ işareti koyar
+// // //*4      \b        Backspace           geriye doğru bir karakter siler bazen konsolda tarayıcıdan dolayı ? görünür
+// // //*5      \n        New Line            yeni satıra geçer
+// // //*6       \t       Horizontal Tabulator bir tab boşluk bırakır
+// // let mtn = 'Ali\'nin "evi" ve arabası\\ çok güzel.\b Ali\n okula\t gidiyor.';
+// // console.log(mtn);
+//ÖRNEK
+//text-1 de verilen "hello world ifadesini tersten yazdırınız.//dlrow olleh"
+// let text1 = "hello world";
+// for (let i = text1.length - 1; i >= 0; i--) {
+//   console.log(text1[i]);
+//   console.log(" ");
+// }
+// //YANYANA ÇÖZÜM
+// let ters = "";
+// for (let i = text1.length - 1; i >= 0; i--) {
+//   ters = ters + text1[i];
+// }
+// console.log(ters);
+
+let text2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+let sonuc = "";
+sonuc = text2.toLowerCase(); // tüm texti küçük harfe çevirir
+sonuc = text2.toUpperCase(); //tüm texti büyük harfe çevirir
+sonuc = text2.length; //textin uzunluğunu verir
+sonuc = text2.length - 1; //textin kaç indexli olduğunu verir
+sonuc = text2[0]; //L
+sonuc = text2[1];//o
+
+
+
+
+
+
