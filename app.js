3009fbd27688633cc15ed2070d15648c33d95b40
CMLTTN <cemaldim@gmail.com>
2023-04-25 10:04:36 +0300

    nested

--- app.js
+++ app.js
@@ -216,3 +216,31 @@ const person = worker;
 person.dob = 20000;
 console.log({person});
 console.log({worker});
+
+const people = {
+  person1: {
+    name: "Can",
+    surname: "Canan",
+    dob: "1990",
+    job: "developer",
+    salary: "140000",
+    drivingLicense: true,
+  },
+  person2: {
+    name: "John",
+    surname: "Sweet",
+    dob: "1990",
+    job: "tester",
+    salary: "110000",
+    drivingLicense: false,
+  },
+  person3: {
+    name: "Steve",
+    surname: "Job",
+    dob: "2000",
+    job: "developer",
+    salary: "90000",
+    drivingLicense: true,
+  },
+  person4: "JS",
+};
