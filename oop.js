015a69bf22257f6889071c9ec60a2ee1ba33acbb
CMLTTN <cemaldim@gmail.com>
2023-05-02 21:09:04 +0300

    classes

--- oop.js
+++ oop.js
@@ -55,3 +55,47 @@ Magazine.prototype = Object.create(Book.prototype);
 const mag11 = new Magazine("SRE", "Einstein", 1930, "Nov");
 console.log(mag11);
 console.log(mag11.getSummary());
+
+//? classes inheritance
+
+class Book1 {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    // ? Bu alanda yazilan bir metot butun instance'ların belleginde tek tek yer kaplar.
+    //  this.getTitle = function () {
+    //    return this.title
+    //  }
+  }
+  getSummary() {
+    return `${this.title} was writtten by ${this.author} in ${this.year}`;
+  }
+  getAge() {
+    return `${new Date().getFullYear() - this.year}`;
+  }
+}
+const book1 = new Book1("Kasagi", "Ömer Seyfettin", 1920);
+console.log(book1);
+console.log(book1.getSummary());
+console.log(book1.getAge());
+const book2 = new Book1("Simyaci", "Pauolo Coelho", 1990);
+console.log(book2.getSummary());
+console.log(book2.getAge());
+class Magazine1 extends Book1 {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+}
+const mag1 = new Magazine1("Sre", "Einstein", 1930, "Nov");
+console.log(mag1);
+console.log(mag1.getAge());
+console.log(mag1.getSummary());
+
+
+
+
+
+
+
